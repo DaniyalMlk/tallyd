d7e034a7010e1e490d4adcb4bc7f1938be1df468
Daniyal <103329154+DaniyalMlk@users.noreply.github.com>
2026-08-19 02:19:38 +0000

    chore(group): export the elimination engine from the barrel

--- src/group/index.ts
+++ src/group/index.ts
@@ -9,6 +9,19 @@ export {
   type HoldingDefinition,
   UnknownEntityError,
 } from "./structure.js";
+export { CONSOLIDATION_ACCOUNTS, GROUP_ACCOUNTS, groupChart } from "./accounts.js";
+export {
+  type EliminationOptions,
+  type Eliminations,
+  type IntercompanyDeclaration,
+  type IntercompanyPair,
+  type IntercompanySide,
+  type PairKind,
+  type UnmatchedDeclaration,
+  eliminateIntercompany,
+  intercompanyBalances,
+  renderEliminations,
+} from "./intercompany.js";
 export {
   type Aggregation,
   type AggregationOptions,
